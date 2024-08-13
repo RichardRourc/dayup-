f56d487585e2d9fe1a321a8077c7501065fa34cc
RichardRourc <13640140783@163.com>
2024-08-13 13:01:07 +0800

    update: 更新单选日期组件

--- 一些组件/单独选开始和结束日期组件/DatePickerDateSingle.vue
+++ 一些组件/单独选开始和结束日期组件/DatePickerDateSingle.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="daterangeSelect" @mouseenter="checkData" @mouseleave="hideClose">
+  <div
+    class="daterangeSelect"
+    @mouseenter="checkDateData"
+    @mouseleave="hideClose"
+  >
     <el-date-picker
       v-model="startValue"
       class="currentPick monthStyle daterange--start"
@@ -9,7 +13,7 @@
       placeholder="开始时间"
       :disabled="disabled"
       :clearable="false"
-      @change="pickRangeBtn"
+      @change="onBtnClick"
       :value-format="valueFormatStart"
       :format="formatStart"
     />
@@ -25,244 +29,246 @@
       :clearable="false"
       :value-format="valueFormatEnd"
       :format="formatEnd"
-      :disabled="disabled || startPeriodType == 'long' || (startPeriodType == 'limited' && !startValue)"
-      @change="pickRangeBtn"
+      :disabled="
+        disabled ||
+        startPeriodType == 'long' ||
+        (startPeriodType == 'limited' && !startValue)
+      "
+      @change="onBtnClick"
     />
-    <i class="icon-clear-date" v-if="!disabled && closeVisible" @click="clearDate"></i>
+    <i
+      class="icon-clear-date"
+      v-if="!disabled && closeVisible"
+      @click="clearDate"
+    ></i>
   </div>
 </template>
 
 <script>
-import moment from 'moment';
+import moment from "moment";
 
-import _isEqual from 'lodash/isEqual';
-import cloneDeep from 'lodash/cloneDeep';
+import _isEqual from "lodash/isEqual";
+import cloneDeep from "lodash/cloneDeep";
 
 export default {
   props: {
     // 禁用
     disabled: {
       type: Boolean,
-      default: false
+      default: false,
     },
     // 选中的回调
     selectcall: {
       type: Function,
-      default: function() {}
+      default: function () {},
     },
     // 期间类型
     startPeriodType: {
       type: String,
-      default: 'limited' // limited-固定期间 long-长期
+      default: "limited", // limited-固定期间 long-长期
     },
     // 绑定的值
     value: {
       type: Array,
-      default: () => ['', '']
+      default: () => ["", ""],
     },
     // 是否需要从今天开始选
     startFromToday: {
       type: Boolean,
-      default: false
+      default: true,
     },
     // 是否可以清空
     canClear: {
       type: Boolean,
-      default: false
+      default: false,
     },
     dateType: {
       type: String,
-      default: 'date'
+      default: "date",
     },
     formatStartString: {
       type: String,
-      default: 'yyyy-MM-dd'
+      default: "yyyy-MM-dd",
     },
     formatEndString: {
       type: String,
-      default: 'yyyy-MM-dd'
-    }
-  },
-  data() {
-    return {
-      // 关闭按钮是否显示
-      closeVisible: false,
-      queryStartValue: ['', ''],
-      startValue: '',
-      endValue: '',
-      pickerOptionsStart: {
-        firstDayOfWeek: 1,
-        disabledDate: time => {
-          let nowTime = '';
-          // 小于当天的都不可以选吧
-
-          // 设置一个属性，当天以前的是否可选
-          if (this.startFromToday) {
-            if (
-              time.getTime() <
-              moment()
-                .startOf('day')
-                .valueOf()
-            ) {
-              return true;
-            }
-          }
-
-          // 如果有选中结束日期了，需要小于等于选中日期
-          if (parseInt(this.value[1])) {
-            nowTime = moment(this.value[1]);
-
-            return time.getTime() > nowTime.startOf('day').valueOf();
-          }
-        }
-      },
-      pickerOptionsEnd: {
-        firstDayOfWeek: 1,
-        // 目前是这个月的第一天以及今天，实际上应该是上一个日期以及后面？总之后面不设限？可是如果第一个还没选呢？我也disabled?
-        disabledDate: time => {
-          let nowTime = '';
-
-          if (this.startFromToday) {
-            // 小于当天的不可选
-            if (
-              time.getTime() <
-              moment()
-                .startOf('day')
-                .valueOf()
-            ) {
-              return true;
-            }
-          }
-
-          // 如果有选中开始日期了，需要大于等于选中日期
-          if (parseInt(this.value[0])) {
-            nowTime = moment(this.value[0]);
-
-            return time.getTime() < nowTime.startOf('day').valueOf();
-          }
-        }
-      }
-    };
+      default: "yyyy-MM-dd",
+    },
   },
   computed: {
-    valueFormatEnd() {
-      if (this.startPeriodType == 'long') {
-        return '-';
-      } else {
-        return this.formatEndString;
-      }
+    // computed 里面放一个函数搞不起来啊，没响应的？要用到变量和模板化的话，还是函数赋值给变量好？。可以的。。。是computed书写不规范，其他地方写了。。。
+    pickerOptionsStart() {
+      return this.generatePickerOption(
+        "start",
+        this.startFromToday,
+        this.value
+      );
     },
-    formatEnd() {
-      if (this.startPeriodType == 'long') {
-        return '-';
-      } else {
-        return this.formatEndString;
-      }
+    pickerOptionsEnd() {
+      return this.generatePickerOption("end", this.startFromToday, this.value);
     },
+
     valueFormatStart() {
-      if (this.startPeriodType == 'long') {
-        return '-';
+      if (this.startPeriodType == "long") {
+        return "-";
       } else {
         return this.formatStartString;
       }
     },
     formatStart() {
-      if (this.startPeriodType == 'long') {
-        return '-';
+      if (this.startPeriodType == "long") {
+        return "-";
       } else {
         return this.formatStartString;
       }
-    }
-  },
-  watch: {
-    startPeriodType(v) {
-      if (v == 'long') {
-        this.endValue = '-';
+    },
+
+    valueFormatEnd() {
+      if (this.startPeriodType == "long") {
+        return "-";
       } else {
-        this.endValue = '';
+        return this.formatEndString;
       }
     },
-    // TODO 为了模拟v-model，除了emit向上改变，还需要上到下，下层监听prop改变，并判断两者是否相等，然后再赋值
+    formatEnd() {
+      if (this.startPeriodType == "long") {
+        return "-";
+      } else {
+        return this.formatEndString;
+      }
+    },
+  },
+  data() {
+    return {
+      // 关闭按钮是否显示
+      closeVisible: false,
+      queryStartValue: ["", ""], // 用于给外部回调函数当数据用 ????
+      startValue: "",
+      endValue: "",
+    };
+  },
+
+  watch: {
+    startPeriodType(v) {
+      if (v == "long") {
+        this.endValue = "-";
+      } else {
+        this.endValue = "";
+      }
+    },
+    // 为了模拟v-model双向修改，除了emit向上改变，还需要上到下，下层监听prop改变，并判断两者是否相等，然后再赋值
     value: {
       deep: true,
       // immediate: true,
       handler(v, ov) {
-        console.log(v, ov, this.startValue, this.endValue, 'value');
         if (!_isEqual(v, [this.startValue, this.endValue])) {
           this.startValue = cloneDeep(v[0]);
           this.endValue = cloneDeep(v[1]);
         }
-      }
-    }
+      },
+    },
   },
   created() {
-    this.setQueryDate('daterange');
+    this.initDateData();
   },
   methods: {
-    clearDate() {
-      this.startValue = '';
-      this.endValue = '';
-      this.$emit('update:value', ['', '']);
-      this.selectcall({ queryType: 'daterange', queryStartValue: '', queryEndValue: '', dateStr: 'daterange' });
+    // 生成开始和结束的pickerOption选项
+    generatePickerOption(type, startFromToday, value) {
+      let resObj = {};
+      resObj.firstDayOfWeek = 1;
+      resObj.disabledDate = function (time) {
+        let nowTime = "";
 
-      // 关闭自己
-      this.closeVisible = false;
+        // 设置一个属性，当天以前的是否可选
+        if (startFromToday) {
+          if (time.getTime() < moment().startOf("day").valueOf()) {
+            return true;
+          }
+        }
+
+        const isStart = type == "start" ? true : false;
+        const compareValue = isStart ? value[1] : value[0];
+
+        if (parseInt(compareValue)) {
+          nowTime = moment(compareValue);
+          // 如果有选中结束日期了，需要小于等于选中日期
+          if (isStart) {
+            return time.getTime() > nowTime.startOf("day").valueOf();
+          } else {
+            // 如果有开始日期了，需要大于等于选中日期
+            return time.getTime() < nowTime.startOf("day").valueOf();
+          }
+        }
+      };
+
+      return resObj;
+    },
+    clearDate() {
+      this.startValue = "";
+      this.endValue = "";
+      this.$emit("update:value", ["", ""]);
+      this.selectcall({
+        queryType: "daterange",
+        queryStartValue: "",
+        queryEndValue: "",
+        dateStr: "daterange",
+      });
+
+      // 关闭清空图标的显示
+      this.hideClose();
     },
     hideClose() {
       this.closeVisible = false;
     },
-    checkData() {
+    // 鼠标划入时判断组件是否有数据，有数据则显示可清空按钮
+    checkDateData() {
       if (Array.isArray(this.value)) {
-        if (this.startValue && this.startValue !== 'Invalid date') {
+        if (this.startValue && this.startValue !== "Invalid date") {
           this.closeVisible = true;
-        } else if (this.endValue && this.endValue !== 'Invalid date') {
+        } else if (this.endValue && this.endValue !== "Invalid date") {
           this.closeVisible = true;
         }
       }
     },
-    getDafaultDate(dateType) {
-      return this.defaultValArray ? this.defaultValArray[this.dateArray.findIndex(_ => _ === dateType)] : '';
+
+    initDateData() {
+      this.setQueryDate();
     },
-    // 这个设置是为了啥，设置初始值
-    setQueryDate(dateType, value) {
+    // 初始化数据或者回显数据处理
+    setQueryDate() {
       // 进来没有默认值，除非有设置才默认值设置
 
-      if (dateType === 'daterange') {
-        // 进来如果要回显的话，说明第一个必须是要有值的
-        // TODO 不知道为啥空字符串也可以判定进来了
-        if (parseInt(this.value[0]) || parseInt(this.value[1])) {
-          // 如果有值时的回显
-          console.log(this.value[0], 'this.value[0]');
-          if (parseInt(this.value[0])) {
-            this.startValue = moment(this.value[0]).format(this.formatStartString);
-          }
+      // 进来如果要回显的话，说明第一个必须是要有值的
+      if (parseInt(this.value[0]) || parseInt(this.value[1])) {
+        // 如果有值时的回显
 
-          console.log(this.startValue, 'startValue');
-          this.queryStartValue[0] = this.startValue;
-          if (parseInt(this.value[1])) {
-            this.endValue = moment(this.value[1]).format(this.formatEndString);
-            this.queryStartValue[1] = this.endValue;
-          }
+        if (parseInt(this.value[0])) {
+          this.startValue = this.queryStartValue[0] =
+            this.replaceToMomentFormatString(this.value[0]);
+        }
+
+        if (parseInt(this.value[1])) {
+          this.endValue = this.queryStartValue[1] =
+            this.replaceToMomentFormatString(this.value[1]);
         }
       }
-
-      this.pickCurrent(this.queryStartValue);
     },
-    pickRangeBtn(val) {
-      console.log('test clear', val);
-
+    // 这里是为了兼容moment的字符串日期格式，moment后面日期是DD。element和浏览器的是dd
+    replaceToMomentFormatString(value) {
+      return moment(value).format(
+        this.formatStartString.replace("yyyy-MM-dd", "yyyy-MM-DD")
+      );
+    },
+    // 监控日期组件按钮点击
+    onBtnClick(val) {
       const { startValue, endValue } = this;
-      // let queryType = this.queryTypes.findIndex(item => filterDateType === item) + 1;
-      const queryType = 'daterange';
-      let queryStartValue = '';
-      let queryEndValue = '';
+
+      let queryStartValue = "";
+      let queryEndValue = "";
 
       // 点击清除按钮的时候，需要把值置为空
       if (!val) {
-        this.startValue = '';
-        this.endValue = '';
-        this.$emit('update:value', ['', '']);
-        this.selectcall({ queryType, queryStartValue, queryEndValue, dateStr: 'daterange' });
+        this.clearDate();
         return false;
       }
 
@@ -275,74 +281,34 @@ export default {
         //   })
         //   return
         // }
-        queryStartValue = moment(startValue).format(this.formatStartString.replace('yyyy-MM-dd', 'yyyy-MM-DD'));
+
+        queryStartValue = this.replaceToMomentFormatString(startValue);
       }
 
       if (endValue) {
-        queryEndValue = moment(endValue).format(this.formatEndString.replace('yyyy-MM-dd', 'yyyy-MM-DD'));
+        queryEndValue = this.replaceToMomentFormatString(endValue);
       }
 
-      console.log(queryStartValue, queryEndValue, 'queryStartValue,queryEndValue 没更新成功吗');
-      // TODO 这里怎么就不用this了
-      this.$emit('update:value', [queryStartValue, queryEndValue]);
+      // 双向绑定修改
+      this.$emit("update:value", [queryStartValue, queryEndValue]);
 
+      // 可能需要用到的回调函数的方式传其他数据
       this.selectcall({
-        queryType,
         queryStartValue,
         queryEndValue,
-        dateStr: 'daterange'
+        dateStr: "daterange",
       });
     },
-
-    pickCurrent(value) {
-      const queryType = 'daterange';
-      let queryStartValue = '';
-      let queryEndValue = '';
-      if (Array.isArray(value)) {
-        // 需要有值且不是无效日期才可以正确赋值
-        if (value[0] && value[0] !== 'Invalid date') {
-          queryStartValue = moment(value[0]).format(this.formatStartString);
-        } else {
-          queryStartValue = '';
-        }
-
-        if (value[1] && value[1] !== 'Invalid date') {
-          queryEndValue = moment(value[1]).format(this.formatEndString);
-        } else {
-          queryEndValue = '';
-        }
-      } else {
-        queryStartValue = moment(value)
-          .clone()
-          .startOf('daterange')
-          .format(this.formatStartString);
-        queryEndValue = moment(value)
-          .clone()
-          .endOf('daterange')
-          .format(this.formatEndString);
-      }
-
-      this.$emit('update:value', [queryStartValue, queryEndValue]);
-      this.selectcall({
-        queryType,
-        queryStartValue,
-        // ...this.showDateText(),
-        queryEndValue,
-        dateStr: 'daterange',
-        initTag: true
-        // ...this.showRate(filterDateType, queryStartValue)
-      });
-    }
-  }
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .daterangeSelect {
   position: relative;
-  height: 0.32rem;
+  height: 32px;
   width: 243px;
-  line-height: 0.32rem;
+  line-height: 32px;
   background-color: #fff;
 
   border-radius: 2px 2px 2px 2px;
@@ -369,7 +335,8 @@ export default {
     margin: auto;
     width: 14px;
     height: 14px;
-    background: url('~@/assets/img/icon-clear-date.svg') no-repeat center;
+    background-color: #9399ae;
+    // background: url('~@/assets/img/icon-clear-date.svg') no-repeat center;
     background-size: 100% 100%;
     cursor: pointer;
     //
@@ -387,42 +354,42 @@ export default {
     width: 110px;
     height: 100%;
 
-    /deep/ .el-input__inner {
+    ::v-deep .el-input__inner {
       border: none !important;
       // width: 82px;
       width: 100% !important;
       height: 95%;
     }
 
-    /deep/ .el-input__icon {
+    ::v-deep .el-input__icon {
       display: flex;
       align-items: center;
     }
   }
 
   .el-date-editor.daterange--start {
-    /deep/ .el-input__inner {
+    ::v-deep .el-input__inner {
       text-indent: 20px;
     }
   }
   .el-date-editor.daterange--end {
     width: 100px;
-    /deep/ .el-input__inner {
+    ::v-deep .el-input__inner {
       width: 100px;
     }
-    /deep/ .el-icon-date:before {
-      content: '';
+    ::v-deep .el-icon-date:before {
+      content: "";
     }
-    /deep/ .el-input__suffix {
+    ::v-deep .el-input__suffix {
       right: 0;
     }
-    /deep/ .el-input__icon {
+    ::v-deep .el-input__icon {
       right: 0;
       width: 12.25px;
     }
   }
 
-  /deep/ .el-input__inner {
+  ::v-deep .el-input__inner {
     padding: 0;
     // width: 5.8rem;
     background-color: transparent !important;
